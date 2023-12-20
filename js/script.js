6259986bccd6a7a3ee97180e82db9f1b8e737d36
Ania <anna.szczolko@gmail.com>
2023-12-20 11:34:21 +0100

    changed js

--- js/script.js
+++ js/script.js
@@ -6,44 +6,57 @@ const body = document.querySelector('#body')
 const spanYear = document.querySelector('.footer__year')
 let year
 
-
 const showMenuList = () => {
+	isNavActive = navList.getAttribute('aria-expanded')
     
-    isNavActive = navList.getAttribute('aria-expanded')
-    
-    if (isNavActive == 'false') {
-        navList.setAttribute('aria-expanded', 'true')
-        burgerIcon.setAttribute('src','./img/icon-close.svg')
-        body.setAttribute('aria-hidden', 'true')
-    } else {
-        navList.setAttribute('aria-expanded', 'false')
-        burgerIcon.setAttribute('src','./img/icon-hamburger.svg')
-        body.setAttribute('aria-hidden', 'false')
-    }
-    linkAnimation()
-    
+	if (isNavActive == 'false') {
+        addlinkAnimation()
+        openMenuList()
+        
+	} else {
+
+		closeMenuList()
+	}
 }
 
+const openMenuList = () => {
+    navList.setAttribute('aria-expanded', 'true')
+    burgerIcon.setAttribute('src', './img/icon-close.svg')
+    body.setAttribute('aria-hidden', 'true')
+}
 
-const linkAnimation = () => {
+const closeMenuList = () => {
+	navList.setAttribute('aria-expanded', 'false')
+	burgerIcon.setAttribute('src', './img/icon-hamburger.svg')
+	body.setAttribute('aria-hidden', 'false')
 
-	allNavItems.forEach((navItem, index) => {
-        navItem.classList.toggle('link-animation')
-    
-    })
+    removelinkAnimation()
+}
+
+const addlinkAnimation = () => {
+    console.log('add anim');
+	allNavItems.forEach(navItem => {
+		navItem.classList.add('link-animation')
+	})
+}
+
+const removelinkAnimation = () => {
+    console.log('remove anim');
+	allNavItems.forEach(navItem => {
+		navItem.classList.remove('link-animation')
+	})
 }
 
 const showYear = () => {
-  data = new Date
-  year = data.getFullYear()
-  spanYear.textContent = year;
-
+	data = new Date()
+	year = data.getFullYear()
+	spanYear.textContent = year
 }
 
 showYear()
+
 burgerBtn.addEventListener('click', showMenuList)
 
 allNavItems.forEach(navItem => {
-	navItem.addEventListener('click', showMenuList)
+	navItem.addEventListener('click', closeMenuList)
 })
-
